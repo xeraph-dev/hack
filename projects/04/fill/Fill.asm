2169d0e7b11e1aac438951db63ca3e0a264bdf91
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-12-09 21:24:18 -0500

    project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,58 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+  @SCREEN
+  D=A
+  @R0
+  M=D
+
+  (WHITE)
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+
+    @R0
+    D=M
+
+    @KBD
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    @R0
+    A=M
+    M=0
+
+    @R0
+    M=M+1
+
+    @WHITE
+    0;JEQ
+  
+  (FILL)
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+
+    @R0
+    D=M
+
+    @KBD
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    @1
+    D=-A
+    @R0
+    A=M
+    M=D
+
+    @R0
+    M=M+1
+
+    @FILL
+    0;JEQ
